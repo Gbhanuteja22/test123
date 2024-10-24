145f738de4b5c653a3b462bc3ef8d7103d10ed3e
Gummadavelli Bhanu Teja <bhanutejagummadevelli@gmail.com>
2024-10-24 06:51:34 +0530

    Update PaymentServlet.java

--- WEB-INF/classes/PaymentServlet.java
+++ WEB-INF/classes/PaymentServlet.java
@@ -1,12 +1,10 @@
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-
 public class PaymentServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-
         out.println("<!DOCTYPE html>");
         out.println("<html lang='en'>");
         out.println("<head>");
