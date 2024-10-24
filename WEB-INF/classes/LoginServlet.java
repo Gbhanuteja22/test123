bf92b224f0e1e491ba544f8a426048b7b5fd831c
Gummadavelli Bhanu Teja <bhanutejagummadevelli@gmail.com>
2024-10-24 06:51:16 +0530

    Update LoginServlet.java

--- WEB-INF/classes/LoginServlet.java
+++ WEB-INF/classes/LoginServlet.java
@@ -1,15 +1,12 @@
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String userId = request.getParameter("userid");
         String password = request.getParameter("password");
-
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-
         if (RegistrationServlet.isUserValid(userId, password)) {
             HttpSession session = request.getSession();
             session.setAttribute("userId", userId);
